bf9b91d766cf5dff2cdb2f916491672d4d081230
Rory O'Keeffe <rorokeeffe@gmail.com>
2016-11-22 18:10:13 +0000

    Expanding things to include selecting users, more component hierarchy, form with onSubmit, etc. Still noobing hard

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-class User extends React.Component {
-  render() {
-    return <li>User: {this.props.user.name}</li>
-  }
-};
-
-class UserList extends React.Component {
+class Users extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       users: [],
       isFetching: false,
-      completedInitialLoad: false
+      completedInitialLoad: false,
+      isCreatingUser: false,
+      selectedUser: null,
     };
 
     this.pollingInterval = false;
 
     // Functions with proper `this` binding
 
-    this._addUser = (evt) => this.addUser(evt);
+    this._addUser = (name, cback) => this.addUser(name, cback);
     this._onFetchUserSuccess = () => this.onFetchUserSuccess();
     this._fetchUsers = () => this.fetchUsers();
+    this._onUserSelected = (user) => this.onUserSelected(user);
   }
 
   // REACT-SPECIFIC FUNCTIONS
 
   componentDidMount() {
-    this._fetchUsers();
+    this.fetchUsers();
 
-    const pollingInterval = setInterval(this._fetchUsers, 5000);
-    this.pollingInterval = pollingInterval;
+    // const pollingInterval = setInterval(this._fetchUsers, 5000);
+    // this.pollingInterval = pollingInterval;
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -75,14 +72,113 @@ class UserList extends React.Component {
       users,
       isFetching: false,
       completedInitialLoad: true,
+      isCreatingUser: false,
     });
   }
 
-  addUser(evt) {
-    const updatedUsers = this.state.users.concat({ name: '(Ci|I|D)an' });
+  addUser(name, cback) {
     this.setState({
-      users: updatedUsers,
+      isCreatingUser: true,
     });
+
+    setTimeout(() => {
+      const updatedUsers = this.state.users.concat({ name })
+      this.setState({
+        users: updatedUsers,
+        isCreatingUser: false,
+      });
+      if(cback) {
+        cback();
+      }
+    }, 1000);
+  }
+
+  onUserSelected(user) {
+    const selectedUser = this.state.selectedUser && user.name === this.state.selectedUser.name ? null : user;
+    this.setState({
+      selectedUser: selectedUser,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <UserList users={ this.state.users } selectedUser={ this.state.selectedUser } isFetching={ this.state.isFetching } completedInitialLoad={ this.state.completedInitialLoad } onUserSelected={ this._onUserSelected } />
+        <UserCreator onClickAddUser={ this._addUser } isCreatingUser={ this.state.isCreatingUser } />
+      </div>
+    )
+  };
+};
+
+class UserCreator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._addUser = (evt) => this.addUser(evt);
+    this._clearName = () => this.clearName();
+  }
+
+  clearName() {
+    this.userNameField.value = "";
+  }
+
+  addUser(evt) {
+    evt.preventDefault();
+
+    let nameInput = this.userNameField;
+    const name = nameInput.value.trim();
+
+    if(name.length > 0) {
+      this.props.onClickAddUser(name, this._clearName);
+    } else {
+      alert("We don't want no scrubs");
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={ this._addUser }>
+        <input type="text" ref={ ref => this.userNameField = ref } />
+        <button type="submit" disabled={ this.props.isCreatingUser }>Add User</button>
+      </form>
+    );
+  };
+}
+
+class User extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._onUserSelected = (evt) => this.onUserSelected(evt);
+  }
+
+  onUserSelected(evt) {
+    this.props.onUserSelected(this.props.user);
+  }
+
+  render() {
+    return <li onClick={ this._onUserSelected }>User: { this.props.user.name } { this.props.selected ? "SELECTED" : "" }</li>
+  };
+};
+
+// Stateless functional component version of user
+const StatelessUser = ({ user, selected, onUserSelected  }) => {
+  return (
+    <div onClick={evt => onUserSelected(user)}>
+      { `${user.name} ${selected ? " SELECTED" : ''}` };
+    </div>
+  )
+}
+
+class UserList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this._onUserSelected = (user) => this.onUserSelected(user);
+  }
+
+  onUserSelected(user) {
+    this.props.onUserSelected(user);
   }
 
   // RENDER FUNCTIONS
@@ -90,13 +186,13 @@ class UserList extends React.Component {
   renderWithUsers() {
     return (
       <ul>
-        { this.state.users.map((user, key) => <User user={user} key={key} />) }
+        { this.props.users.map((user, key) => <User user={ user } key={ key } onUserSelected={ this._onUserSelected } selected={ this.props.selectedUser && this.props.selectedUser.name === user.name }/>) }
       </ul>
     );
   }
 
   renderWithoutUsers() {
-    if(this.state.isFetching || !this.state.completedInitialLoad) {
+    if(this.props.isFetching || !this.props.completedInitialLoad) {
       return (
         <p>Fetching...</p>
       )
@@ -108,18 +204,11 @@ class UserList extends React.Component {
   }
 
   render() {
-    return (
-      <div className="user-list-container">
-        { this.state.users.length > 0 ? this.renderWithUsers() : this.renderWithoutUsers() }
-        <button type="button" onClick={this._addUser}>Add User</button>
-      </div>
-    )
-  }
+    return this.props.users.length > 0 ? this.renderWithUsers() : this.renderWithoutUsers();
+  };
 };
 
 ReactDom.render(
-  <div>
-    <UserList />
-  </div>,
+  <Users />,
   document.getElementById('app')
 );
